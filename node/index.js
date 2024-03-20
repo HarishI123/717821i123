95db033c8f07010ad77c6e9e1095c41de029f4af
717821i123 <717821i123@kce.ac.in>
2024-03-20 11:32:17 +0530

    even number generator

--- node/index.js
+++ node/index.js
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-app.get("/r", (req, res) => {
+app.get("/numbers/rand", (req, res) => {
   // Create an empty array to store the random numbers
   const newNumber = [];
 
@@ -36,5 +36,38 @@ app.get("/r", (req, res) => {
   // Send the object as the response
   res.json(responseObject);
 })
+
+app.get("/numbers/even", (req, res) => {
+  // Create an empty array to store the random numbers
+  const newNumber = [];
+
+  // Define a function to generate a random start number
+  function getRandomStart() {
+    return Math.floor(Math.random() * 100 + 1);
+  }
+
+  // Define a function to check if a number is even
+  function isEven(num) {
+    return num % 2 == 0;
+  }
+
+  // Generate a random start number
+  let startNumber = getRandomStart();
+  if  (!isEven(startNumber)) {
+    startNumber++; // Make sure it'
+  }
+
+  // Generate 10 consecutive even numbers starting from the random start number
+  for (let i = 0; i < 10; i++) {
+    newNumber.push(startNumber);
+    startNumber += 2;
+  }
+
+  // Create an object with a single property called "number" that contains the array of random numbers
+  const responseObject = { number: newNumber };
+
+  // Send the object as the response
+  res.json(responseObject);
+});
 
 app.listen(port, () => console.log(`Server running on http://localhost:${port}/`));
